8f214d8f2fdce3f453a2f6f267d9ee2f7668b136
Simon Schick <simonsimcity@gmail.com>
2011-12-05 22:33:03 +0100

    Updated wrong name for admin-user

--- config.template.php
+++ config.template.php
@@ -5,6 +5,6 @@ return array(
 		'users' => __DIR__.'/htusers' // The file where the users are stored in. This users are managed and are the only one who can login here.
 	),
 	'firewall' => array(
-		'admin' => 'administrator' // Which user should have admin-rights?
+		'admin' => 'admin' // Which user should have admin-rights?
 	)
 );

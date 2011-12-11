342de388eb6851bc044b01cd213957385d428eb5
Simon Schick <simonsimcity@gmail.com>
2011-12-11 13:33:03 +0100

    Updated password-validation to accept all crypt-passwords apache would accept. Implemented password-validation for apr1-md5 and plain-text passwords.

--- Firewall.php
+++ Firewall.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once __DIR__ . "/UserManager.php";
+require_once __DIR__ . "/EncryptionInterface.php";
+require_once __DIR__ . "/EncryptionCrypt.php";
+require_once __DIR__ . "/EncryptionMd5.php";
+require_once __DIR__ . "/EncryptionPlainText.php";
 
 class Firewall {
 
@@ -8,12 +12,34 @@ class Firewall {
 	 * @return bool
 	 */
 	public function isAutenticated() {
-		global $userManager;
+		global $userManager, $config;
 
-		return
-				array_key_exists('PHP_AUTH_USER', $_SERVER) && array_key_exists('PHP_AUTH_PW', $_SERVER) &&
-				$userManager->userExists($_SERVER['PHP_AUTH_USER']) &&
-				$userManager->getCurrentUser()->getPassword() === crypt($_SERVER['PHP_AUTH_PW'], base64_encode($_SERVER['PHP_AUTH_PW']));
+		// If the Server-variables do not exist, the user cannot be validated.
+		if (!array_key_exists('PHP_AUTH_USER', $_SERVER) || !array_key_exists('PHP_AUTH_PW', $_SERVER))
+			return false;
+
+		$username = $_SERVER['PHP_AUTH_USER'];
+
+		// Check if the user exists
+		if (!$userManager->userExists($username))
+			return false;
+
+		// Check if the password can be validated by any enabled encryption class
+		foreach ($config['firewall']['acceptEncryptionClasses'] as $encryptionClass) {
+			/** @var $encryption EncryptionInterface */
+			$encryption = new $encryptionClass;
+			if (!($encryption instanceof EncryptionInterface))
+				throw new Exception('The registered encryption-class <' . get_class($encryption) . '> does not implement the EncryptionInterface');
+
+			$result = $encryption->validateEncryptedString($_SERVER['PHP_AUTH_PW'],
+				$userManager->getUserByName($username)->getPassword());
+			if ($result === true)
+				return true;
+			elseif ($result === false)
+				return false;
+		}
+
+		return false;
 	}
 
 	/**
--- config.template.php
+++ config.template.php
@@ -5,6 +5,14 @@ return array(
 		'users' => __DIR__.'/htusers' // The file where the users are stored in. This users are managed and are the only one who can login here.
 	),
 	'firewall' => array(
+		'acceptEncryptionClasses' => array(
+			'EncryptionCrypt',
+			'EncryptionMd5',
+			//'EncryptionPlainText', Please only enable if really needed
+		),
 		'admin' => 'admin' // Which user should have admin-rights?
+	),
+	'userController' => array(
+		'savePasswordEncryptionClass' => 'EncryptionCrypt' // You can also use EncryptionPlainText or EncryptionMd5
 	)
 );
